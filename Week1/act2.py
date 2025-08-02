b006fe54c644bd93d724c68104c7ef4078cf0488
Alvinc11 <zero470042950@gmail.com>
2025-08-02 15:04:38 +1200

    List all even numbers &Add the sum of odd number.
    1.Using a While loop.
    2.Add list all even numbers.
    3.Add the sum of odd number.
    4.Grammer improved.

--- Week1/act2.py
+++ Week1/act2.py
@@ -1,10 +1,18 @@
 n = int(input("Enter a number to sum even numbers up to: "))
 if n < 1:
-    print("In valid number. Please input a number over or equal than 1:")
+    print("Invalid number. Please input a number greater than or equal to 1:")
 
 else:
-    result = 0
-    for i in range(1, n + 1):
+    odd_sum = 0
+    even_sum = 0
+    i = 1
+    print("All even numbers:")
+    while i <= n:
         if i % 2 == 0:
-            result = result + i
-    print("The sum of even numbers up to", n, "is:", result)
+            print(i, end=" ")
+            even_sum += i
+        else:
+            odd_sum += i
+        i += 1
+    print("\nThe sum of even numbers up to", n, "is:", even_sum)
+    print("The sum of odd numbers up to", n, "is:", odd_sum)

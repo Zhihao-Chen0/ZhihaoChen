335be9bf65abf1065226713bbdd295909a6d833b
Zhihao-Chen0 <270635812@yoobeestudent.ac.nz>
2025-08-10 08:44:22 +1200

    act3 first version

---
+++ Week2/act3 look_for_a_character.py
@@ -0,0 +1,22 @@
+class StringManipulator:
+    def __init__(self, text):
+        self.text = text
+
+    def find_character(self, char):
+        return self.text.find(char)
+
+    def measure_length(self):
+        return len(self.text)
+    
+    def convert_to_uppercase(self):
+        return self.text.upper()
+    
+def main():
+    name = StringManipulator("example")
+
+    print(name.find_character("e"))
+    print(name.measure_length())
+    print(name.convert_to_uppercase())
+
+if __name__ == "__main__":
+    main()

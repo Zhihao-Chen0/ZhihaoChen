7be04bedaf192ca8dd4d4b8ec264562ea7ef5a47
Zhihao-Chen0 <270635812@yoobeestudent.ac.nz>
2025-09-03 16:13:06 +1200

    Finish W5A6.2

---
+++ Week5/act6 encapsulation/6.2 Different_Types_of_Attributes.py
@@ -0,0 +1,49 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name       # public
+        self._age = age        # protected
+        self.__grade = 'A'     # private
+
+    def get_grade(self):
+        return self.__grade
+
+    #6.2 Update grade
+    def update_grade(self, new_grade):
+        self.__grade = new_grade
+
+    def grade_info(self):
+        return f"{self.name}'s grade is {self.__grade}"
+
+class Teacher:
+    def __init__(self, name, subject):
+        self.name = name       # public
+        self._subject = subject # protected
+        self.__salary = 50000   # private
+
+    def get_salary(self):
+        return self.__salary
+
+    def need_raise(self):
+        return self.__salary < 60000
+    
+    def display_info(self):
+        return "Need a raise!" if self.need_raise() else "Salary is sufficient."
+
+s = Student('Ali', 20)
+print(s.name)         # accessible
+print(s._age)         # discouraged
+print(s.get_grade())  # correct way
+print(s.grade_info())  # correct way
+
+#6.2 Update grade
+s.update_grade('A+')
+print("Update:")
+print(s.grade_info())  # correct way after update
+
+print("------------------------------")
+
+t = Teacher('Mr. John', 'Mathematics')
+print(t.name)          # accessible
+print(t._subject)     # discouraged
+print(t.get_salary())  # correct way
+print(t.display_info()) # correct way

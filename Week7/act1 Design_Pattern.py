3f96ccfb1c5d98aa8ad962ccc0285925e7d39fa7
Zhihao-Chen0 <270635812@yoobeestudent.ac.nz>
2025-09-13 12:10:36 +1200

    Create act1 Design_Pattern.py

---
+++ Week7/act1 Design_Pattern.py
@@ -0,0 +1,70 @@
+import sqlite3
+import time
+def init_database():
+    conn = sqlite3.connect('app.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            email TEXT
+        );
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS orders (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            product TEXT,
+            amount REAL
+        );
+    ''')
+
+    cursor.execute("INSERT INTO users (name, email) VALUES (?, ?)", ('Alice', 'alice@example.com'))
+    cursor.execute("INSERT INTO orders (user_id, product, amount) VALUES (?, ?, ?)", (1, 'Book', 19.99))
+    cursor.execute("INSERT INTO orders (user_id, product, amount) VALUES (?, ?, ?)", (1, 'Pen', 1.50))
+
+    conn.commit()
+    conn.close()
+    print("Database initialized.")
+
+class UserService:
+    def get_user(self, user_id):
+        start_time = time.time()
+        conn = sqlite3.connect('app.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
+        result = cursor. fetchone()
+        conn.close()
+        end_time = time.time()
+        print(f"get_user use: {end_time - start_time:.6f} second")
+        return result
+    
+class OrderService:
+    def get_orders(self, user_id):
+        start_time = time.time()
+        conn = sqlite3.connect('app.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM orders WHERE user_id = ?", (user_id,))
+        result = cursor. fetchall()
+        conn.close()
+        end_time = time.time()
+        print(f"get_orders use: {end_time - start_time:.6f} second")
+        return result
+    
+if __name__ == "__main__":
+    init_database()
+    user_service = UserService()
+    order_service = OrderService()
+    
+    user_id = 1
+    user = user_service.get_user(user_id)
+    print("User:", user)
+
+    order = order_service.get_orders(user_id)
+    print("Order:", order)
+
+
+
+    

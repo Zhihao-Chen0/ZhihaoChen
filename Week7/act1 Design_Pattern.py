2de11d2566a2a3d0ab4092fb2577fccf0cc23936
Zhihao-Chen0 <270635812@yoobeestudent.ac.nz>
2025-09-13 23:16:22 +1200

    Update act1 Design_Pattern.py

--- Week7/act1 Design_Pattern.py
+++ Week7/act1 Design_Pattern.py
@@ -1,14 +1,29 @@
 import sqlite3
 import time
+
+ # part2 create a singleton pattern
+class DatabaseConnection:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(DatabaseConnection, cls).__new__(cls)
+            cls._instance.conn = sqlite3.connect('app.db')
+        return cls._instance
+
+    def get_connection(self):
+        return self.conn
+
 def init_database():
-    conn = sqlite3.connect('app.db')
+    db_instance = DatabaseConnection()
+    conn = db_instance.get_connection() # part2 Use singleton to get connection
     cursor = conn.cursor()
 
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            email TEXT
+        name TEXT,
+        email TEXT
         );
     ''')
 
@@ -26,17 +41,18 @@ def init_database():
     cursor.execute("INSERT INTO orders (user_id, product, amount) VALUES (?, ?, ?)", (1, 'Pen', 1.50))
 
     conn.commit()
-    conn.close()
     print("Database initialized.")
+
 
 class UserService:
     def get_user(self, user_id):
         start_time = time.time()
-        conn = sqlite3.connect('app.db')
+        db_instance = DatabaseConnection()
+        conn = db_instance.get_connection() # part2 Use singleton to get connection
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
-        result = cursor. fetchone()
-        conn.close()
+        result = cursor.fetchone()
+
         end_time = time.time()
         print(f"get_user use: {end_time - start_time:.6f} second")
         return result
@@ -44,11 +60,12 @@ class UserService:
 class OrderService:
     def get_orders(self, user_id):
         start_time = time.time()
-        conn = sqlite3.connect('app.db')
+        db_instance = DatabaseConnection()
+        conn = db_instance.get_connection()
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM orders WHERE user_id = ?", (user_id,))
-        result = cursor. fetchall()
-        conn.close()
+        result = cursor.fetchall()
+
         end_time = time.time()
         print(f"get_orders use: {end_time - start_time:.6f} second")
         return result
@@ -65,6 +82,5 @@ if __name__ == "__main__":
     order = order_service.get_orders(user_id)
     print("Order:", order)
 
-
-
-    
+    DatabaseConnection().get_connection().close()
+    print("Database connection closed.")

92f826e430a7f6cc3de9a1f455f72d6f43c1b534
Zhihao-Chen0 <270635812@yoobeestudent.ac.nz>
2025-10-18 11:50:25 +1300

    Update code for act1.1 and act1.2

--- Week12/act1_flask/app.py
+++ Week12/act1_flask/app.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Hello Flask!</h1>"
+    return '''
+    <h1>Hello Flask!</h1>
+     <p>Try visiting <a href="/cal/5">/cal/5</a> to see the square of 5.</p>'''
+
+@app.route('/username/<name>')
+def show_username(name):
+    return f"<h1>{name} is learning Flask!</h1>"
+
+@app.route('/cal/<int:number>')
+def show_square(number):
+    return f"<h1>The square of {number} is {number**2}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
